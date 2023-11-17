1d876eae17cdcfb0ea014b82bc3ef758d8feeffa
Jemd15 <x_jemd_x@hotmail.com>
2023-11-18 12:24:25 +1300

    basic pages defined

---
+++ src/app/pages/new-game/new-game.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-new-game',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './new-game.component.html',
+  styleUrl: './new-game.component.css'
+})
+export class NewGameComponent {
+
+}
---
+++ src/app/pages/playing-game/playing-game.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-playing-game',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './playing-game.component.html',
+  styleUrl: './playing-game.component.css'
+})
+export class PlayingGameComponent {
+
+}
