f8f04c2a8197fda20b4df538957fcf2965b3cfde
Jemd15 <x_jemd_x@hotmail.com>
2023-11-18 12:40:43 +1300

    basic components defined

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AccountComponent } from './pages/account/account.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SigninComponent } from './pages/signin/signin.component';
+import { NewGameComponent } from './pages/new-game/new-game.component';
+import { PlayingGameComponent } from './pages/playing-game/playing-game.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent, pathMatch: 'full' },
+  { path: 'account', component: AccountComponent, pathMatch: 'full' },
+  { path: 'login', component: LoginComponent, pathMatch: 'full' },
+  { path: 'signin', component: SigninComponent, pathMatch: 'full' },
+  { path: 'new-game', component: NewGameComponent, pathMatch: 'full' },
+  { path: 'playing-game', component: PlayingGameComponent, pathMatch: 'full' }
+];
